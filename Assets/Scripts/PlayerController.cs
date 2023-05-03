baab02b256895f8478dcb8ed58fd643e72f73506
Joseph-Macalinao <jhmac121101@gmail.com>
2023-05-02 23:09:04 -0700

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,12 @@ public class PlayerController : MonoBehaviour
     public float turnSpeed = 20f;
     float m_timer = 0.0f;
     float m_idleTime = 0.4f;
+    public float jumpHeight = 2f;
+    private bool isGrounded;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
+    public Vector3 jump;
     Quaternion m_Rotation = Quaternion.identity;
 
 
@@ -18,10 +21,11 @@ public class PlayerController : MonoBehaviour
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -49,9 +53,10 @@ public class PlayerController : MonoBehaviour
             m_Animator.SetBool("IsWalking", true);
             m_timer = 0f;
         }
-        if(Attack)
+        //if(Attack)
+        if(Input.GetMouseButtonDown(0))
         {
-            m_timer += Time.deltaTime;
+            //m_timer += Time.deltaTime;
             /*if(m_timer >= m_idleTime)
             {
                 m_Animator.SetBool("Attack", false);
@@ -59,12 +64,21 @@ public class PlayerController : MonoBehaviour
             }
         } else*/
         
-            m_Animator.SetTrigger("Attack");
-            m_timer = 0f;
+            m_Animator.SetBool("Attack", true);
+            //m_timer = 0f;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            m_Rigidbody.AddForce(jump * jumpHeight, ForceMode.Impulse);
+            isGrounded = false;
         }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnCollisionStay(){
+        isGrounded = true;
     }
 
     void OnAnimatorMove()
