c95232e61d067f16af1e03b34345232185bcfee4
Joseph-Macalinao <jhmac121101@gmail.com>
2023-05-09 22:20:05 -0700

    fix small

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -50,10 +50,7 @@ public class PlayerController : MonoBehaviour
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
-        //bool checkAttack = 
-        //bool Attack = Input.GetMouseButtonDown(0);
         m_Animator.SetBool("IsWalking", isWalking);
-        //m_Animator.SetBool("Attack", Attack); 
         if(!isWalking)
         {
             m_timer += Time.deltaTime;
@@ -67,19 +64,9 @@ public class PlayerController : MonoBehaviour
             m_Animator.SetBool("IsWalking", true);
             m_timer = 0f;
         }
-        //if(Attack)
         if(Input.GetMouseButtonDown(0))
         {
-            //m_timer += Time.deltaTime;
-            /*if(m_timer >= m_idleTime)
-            {
-                m_Animator.SetBool("Attack", false);
-                m_timer = 0f;
-            }
-        } else*/
-        
             m_Animator.Play("Attack");
-            //m_timer = 0f;
         }
 
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
@@ -102,7 +89,7 @@ public class PlayerController : MonoBehaviour
 
     void OnAnimatorMove()
     {
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * movementMultiplier);//m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * movementMultiplier);
         m_Rigidbody.MoveRotation(m_Rotation);
     }
 
@@ -111,22 +98,12 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision other){
-
-
-        /*if(other.gameObject.CompareTag("Human")){
-            Debug.Log("ype");
-            other.gameObject.SetActive(false);
-
-            //WaveSpawner.spawnedEnemies.Count--;
-  
-        }*/
         ContactPoint contact = other.contacts[0];
         if(other.gameObject.CompareTag("Human")){
             count = count + 1;
             Destroy(other.gameObject);
             
             SetConqueredText();
-            //WaveSpawner.spawnedEnemies.Count--;
         }
     }
 
