23e24b0c9c8230cdd1422c2cde697bb0ec1f75d3
Joseph-Macalinao <jhmac121101@gmail.com>
2023-05-14 21:51:52 -0700

    orbit

---
+++ Assets/Scripts/cameraOrbit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraOrbit : MonoBehaviour
+{
+    public Transform Player;
+    public float MouseSpeed = 3; 
+    public float orbitDamping = 10;
+
+    Vector3 localRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.position;
+        localRot.x += Input.GetAxis("Mouse X") * MouseSpeed;
+        localRot.y -= Input.GetAxis("Mouse Y") * MouseSpeed;
+        
+
+        localRot.y = Mathf.Clamp(localRot.y, 0f, 80);
+
+        Quaternion QT = Quaternion.Euler(localRot.y, localRot.x, 0f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, QT, Time.deltaTime * orbitDamping);
+    }
+}
