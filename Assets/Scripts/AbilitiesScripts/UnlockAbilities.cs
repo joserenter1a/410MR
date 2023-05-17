ea4decd35d7894b2116c6e9ffdf6fe6e92eb9670
cameronoconnor <coconno2@uoregon.edu>
2023-05-17 04:12:23 -0700

    plz work

---
+++ Assets/Scripts/AbilitiesScripts/UnlockAbilities.cs
@@ -0,0 +1,25 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockAbilities : MonoBehaviour
+{
+    public RawImageController rawImageController;
+    public SprintController sprintController;
+
+    private bool abilitiesUnlocked = false;
+
+    private void Update()
+    {
+        if (!abilitiesUnlocked && rawImageController.RawImageEnabled())
+        {
+            
+        }
+    }
+
+    private void EnableAbilities()
+    {
+        abilitiesUnlocked = true;
+        sprintController.EnableSprint();
+    }
+}*/
---
+++ Assets/Scripts/ChestScripts/RawImageController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RawImageController : MonoBehaviour
+{
+    public RawImage rawImage;
+    //public Component SprintController
+    public GameObject chest;
+    public GameObject player;
+    public float interactionDistance = 4f;
+
+    private SprintController myScript;
+
+    private void Start()
+    {
+        myScript = player.GetComponent<SprintController>();
+
+        rawImage.enabled = false;
+        myScript.enabled = false;
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && IsPlayerNearChest())
+        {
+            rawImage.enabled = !rawImage.enabled;
+            myScript.enabled = true;
+            //gameObject.GetComponent<SprintController>().enabled = true;
+        }
+
+    }
+
+    private bool IsPlayerNearChest()
+    {
+        if (Vector3.Distance(player.transform.position, chest.transform.position) <= interactionDistance)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
